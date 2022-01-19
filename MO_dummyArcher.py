c7b75bd36abe5307312b850759113d0d1f8a481e
DrixnFS <krzyzanek.martin@gmail.com>
2022-01-19 23:37:20 +0100

    - fixed useless line of code

--- MO_dummyArcher.py
+++ MO_dummyArcher.py
@@ -91,7 +91,6 @@ class DummyArcher:
                 pyautogui.press(self.draw_bow_key)
                 time.sleep(2)
                 self.startShooting()
-                self.__current_dura = self.bow_dura
             else:
                 print('--All spare bows have already been used, stopping the script--')
                 os._exit(0)
