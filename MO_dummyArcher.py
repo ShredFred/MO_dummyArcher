6ebb595a89d6169849e12f78c28ede966d09bcda
DrixnFS <krzyzanek.martin@gmail.com>
2022-01-19 22:37:51 +0100

    - lowered the downtime sequence as its not needed

--- MO_dummyArcher.py
+++ MO_dummyArcher.py
@@ -8,7 +8,7 @@ class DummyArcher:
         self.bow_dura = 70 #Set durability of ALL your current and spare bows
         self.spare_bow_key = ['ALT+!', 'ALT+@'] #Set keys under which are your spare bows mappes
         self.draw_bow_key = 'x' # Set key which is used to draw a weapon
-        self.sequence_downtime = 5 # Set a time to wait between each attack sequence and equiping arrows sequence
+        self.sequence_downtime = 1 # Set a time to wait between each attack sequence and equiping arrows sequence
 
 
         # !!!! DO NOT EDIT ANYTHING UNDER THIS LINE IF YOU DO NOT PERFECTLY KNOW HOW EVERYTHING WORKS, ONLY CHANGE THE VARIABLES ABOVE TO ALTER THE BEHAVIOR !!! #
